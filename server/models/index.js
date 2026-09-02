d30206bf22baa57cbc6881d338d5684ebd5b85bf
YogitaBhikajiPtil <patilyogita0303@gmail.com>
2026-09-02 09:33:02 +0530

    Fix review model import

--- server/models/index.js
+++ server/models/index.js
@@ -11,7 +11,7 @@ const Payment = require("./Payment");
 const Cart = require("./Cart");
 const CartItem = require("./CartItem");
 const Wishlist = require("./Wishlist");
-const Review = require("./reviewModel");
+const Review = require("./review");
 const Notification = require("./Notification");
 const Message = require("./Message");
 
